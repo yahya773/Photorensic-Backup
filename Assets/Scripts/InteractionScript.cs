f786b971f956f507e938526d1847fcdd74a5263e
ghost <kbinas2022@elam.co.uk>
2024-04-25 12:46:46 +0100

    Detective room

--- Assets/Scripts/InteractionScript.cs
+++ Assets/Scripts/InteractionScript.cs
@@ -1,24 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEditor.Build.Content;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class InteractionScript : MonoBehaviour
 {
 
     public GameObject InteractE;
+    private bool triggerenter;
 
 
     public void Start()
     {
-        InteractE.SetActive(false); 
+        InteractE.SetActive(false);
     }
     private void OnTriggerEnter(Collider other)
     {
-        InteractE.SetActive(true); 
+        InteractE.SetActive(true);
+        Debug.Log("EnterCollider");
+        triggerenter = true;
     }
 
     private void OnTriggerExit(Collider other)
     {
-        InteractE.SetActive(false); 
+        InteractE.SetActive(false);
+        Debug.Log("ExitCollider");
+        triggerenter = false;
     }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && triggerenter)
+        {
+            triggerenter = true; 
+            SceneManager.LoadScene("Cutscene Intro");
+            Debug.Log("ChangeScene"); 
+        }
+    }
+
 }
