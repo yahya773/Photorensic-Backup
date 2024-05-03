6d1b6fa6ffb56d4e26cd0e8428a9ae0dde6d16ba
ghost <kbinas2022@elam.co.uk>
2024-05-03 11:14:28 +0100

    Sliding

--- Assets/Scripts/Sliding Photo Puzzle.cs
+++ Assets/Scripts/Sliding Photo Puzzle.cs
@@ -1,93 +1,92 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SlidingPhotoPuzzle : MonoBehaviour
 {
     [SerializeField] private Transform gameTransform;
-    [SerializeField] private Transform piecePrefab;
+    [SerializeField] private GameObject piecePrefab;
+    [SerializeField] private Sprite puzzleSprite;
 
-    private List<Transform> pieces; 
+    private GameObject[] pieces;
     private int emptyLocation;
     private int size;
-    
+
     private void CreateGamePieces(float gapThickness)
     {
-        float width = 1 / (float)size;
+        float width = 1f / size;
         for (int row = 0; row < size; row++)
         {
             for (int col = 0; col < size; col++)
             {
-                Transform piece = Instantiate(piecePrefab, gameTransform);
+                GameObject piece = Instantiate(piecePrefab, gameTransform);
 
-                piece.localPosition = new Vector3(-1 + (2 * width * col) + width,
-                +1 - (2 * width * col) - width, 0);
+                piece.transform.localPosition = new Vector3(
+                    -1f + (2f * width * col) + width,
+                    1f - (2f * width * row) - width,
+                    0f
+                );
 
-                piece.localScale = ((2 * width) - gapThickness) * Vector3.one;
+                piece.transform.localScale = ((2f * width) - gapThickness) * Vector3.one;
                 piece.name = $"{(row * size) + col}";
 
                 if ((row == size - 1) && (col == size - 1))
                 {
                     emptyLocation = (size * size) - 1;
-                    piece.gameObject.SetActive(false);
+                    piece.SetActive(false);
                 }
-
                 else
                 {
-                    float gap = gapThickness / 2;
-                    Mesh mesh = piece.GetComponent<MeshFilter>().mesh;
-                    Vector2[] uv = new Vector2[4];
+                    SpriteRenderer spriteRenderer = piece.GetComponent<SpriteRenderer>();
+                    spriteRenderer.sprite = puzzleSprite;
 
-                    uv[0] = new Vector2((width * col) + gap, 1 - ((width * (row + 1)) - gap));
-                    uv[1] = new Vector2((width * (col + 1)) - gap, 1 - ((width * (row + 1)) - gap));
-                    uv[2] = new Vector2((width * col) + gap, 1 - ((width * row) + gap));
-                    uv[3] = new Vector2((width * (col + 1)) - gap, 1 - ((width * row) + gap));
-
-                    mesh.uv = uv; 
+                    BoxCollider2D boxCollider = piece.AddComponent<BoxCollider2D>();
+                    boxCollider.size = new Vector2(width, width);
                 }
+
+                pieces[row * size + col] = piece;
             }
         }
     }
+
     void Start()
     {
-        pieces = new List<Transform>(); 
         size = 3;
-        CreateGamePieces(0.01f); 
+        pieces = new GameObject[size * size];
+        CreateGamePieces(0.01f);
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButton(0))
         {
-            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero); 
-            if (hit)
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
             {
-                for (int i = 0; i < pieces.Count; i ++)
+                for (int i = 0; i < pieces.Length; i++)
                 {
-                    if (pieces[i] == hit.transform)
+                    if (pieces[i] == hit.transform.gameObject)
                     {
-                        if (SwapIfValid(i, -size, size)) { break; }
-                        if (SwapIfValid(i, +size, size)) { break; }
-                        if (SwapIfValid(i, -1, 0)) { break; }
-                        if (SwapIfValid(i, +1, size - 1)) { break; }
+                        if (SwapIfValid(i, -size)) break;
+                        if (SwapIfValid(i, size)) break;
+                        if (SwapIfValid(i, -1)) break;
+                        if (SwapIfValid(i, 1)) break;
                     }
                 }
             }
         }
     }
 
-    private bool SwapIfValid(int i, int offset, int colCheck)
+    private bool SwapIfValid(int i, int offset)
     {
-        if (((i % size) != colCheck) && ((i + offset) == emptyLocation))
+        if (((i + offset) >= 0) && ((i + offset) < pieces.Length) && (Mathf.Abs((i + offset) % size - i % size) <= 1))
         {
-            (pieces[i], pieces[i + offset]) = (pieces[i + offset], pieces[i]);
-            (pieces[i].localPosition, pieces[i + offset].localPosition) = ((pieces[i + offset].localPosition, pieces[i].localPosition));
-            emptyLocation = i;
-            return true; 
-
+            if (i + offset == emptyLocation)
+            {
+                (pieces[i], pieces[i + offset]) = (pieces[i + offset], pieces[i]);
+                emptyLocation = i;
+                return true;
+            }
         }
         return false;
-
     }
 }
