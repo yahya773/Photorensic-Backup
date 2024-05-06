5334dd401cfe349be71da663313da779e0da3b03
ghost <kbinas2022@elam.co.uk>
2024-05-06 21:48:11 +0100

    Sliding Puzzle is hurting my brain

--- Assets/Scripts/Sliding Photo Puzzle.cs
+++ Assets/Scripts/Sliding Photo Puzzle.cs
@@ -1,93 +1,129 @@
 using UnityEngine;
-using UnityEngine.UI; 
+using UnityEngine.UI;
 
-public class SlidingPhotoPuzzle : MonoBehaviour
+public class SlidingPuzzle : MonoBehaviour
 {
-    [SerializeField] private Transform gameTransform;
-    [SerializeField] private GameObject piecePrefab;
-    [SerializeField] private Sprite puzzleSprite;
+    public Texture2D puzzleImage; // The image to be used for the puzzle
+    public int puzzleSize = 3; // Size of the puzzle (3x3 by default)
+    public GameObject buttonPrefab; // Prefab for the puzzle piece buttons
+    public Vector2 buttonSize = new Vector2(100f, 100f); // Size of each puzzle piece button
 
-    private GameObject[] pieces;
-    private int emptyLocation;
-    private int size;
-
-    private void CreateGamePieces(float gapThickness)
-    {
-        float width = 1f / size;
-        for (int row = 0; row < size; row++)
-        {
-            for (int col = 0; col < size; col++)
-            {
-                GameObject piece = Instantiate(piecePrefab, gameTransform);
-
-                piece.transform.localPosition = new Vector3(
-                    -1f + (2f * width * col) + width,
-                    1f - (2f * width * row) - width,
-                    0f
-                );
-
-                piece.transform.localScale = ((2f * width) - gapThickness) * Vector3.one;
-                piece.name = $"{(row * size) + col}";
-
-                if ((row == size - 1) && (col == size - 1))
-                {
-                    emptyLocation = (size * size) - 1;
-                    piece.SetActive(false);
-                }
-                else
-                {
-                    SpriteRenderer spriteRenderer = piece.GetComponent<SpriteRenderer>();
-                    spriteRenderer.sprite = puzzleSprite;
-
-                    BoxCollider2D boxCollider = piece.AddComponent<BoxCollider2D>();
-                    boxCollider.size = new Vector2(width, width);
-                }
-
-                pieces[row * size + col] = piece;
-            }
-        }
-    }
+    private Texture2D[] puzzlePieces; // Array to hold individual puzzle pieces
+    private int emptyIndex; // Index of the empty puzzle piece
 
     void Start()
     {
-        size = 3;
-        pieces = new GameObject[size * size];
-        CreateGamePieces(0.1f);
+        // Split the puzzle image into pieces
+        puzzlePieces = SplitImage(puzzleImage, puzzleSize);
+
+        // Shuffle puzzle pieces
+        ShufflePieces();
+
+        // Create puzzle pieces buttons
+        CreateButtons();
     }
 
-    void Update()
+    // Function to split the puzzle image into pieces
+    private Texture2D[] SplitImage(Texture2D image, int size)
     {
-        if (Input.GetMouseButton(0))
+        int pieceWidth = image.width / size;
+        int pieceHeight = image.height / size;
+        Texture2D[] pieces = new Texture2D[size * size];
+
+        for (int i = 0; i < size; i++)
         {
-            RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out hit))
+            for (int j = 0; j < size; j++)
             {
-                for (int i = 0; i < pieces.Length; i++)
-                {
-                    if (pieces[i] == hit.transform.gameObject)
-                    {
-                        if (SwapIfValid(i, -size)) break;
-                        if (SwapIfValid(i, size)) break;
-                        if (SwapIfValid(i, -1)) break;
-                        if (SwapIfValid(i, 1)) break;
-                    }
-                }
+                Texture2D piece = new Texture2D(pieceWidth, pieceHeight);
+                piece.SetPixels(image.GetPixels(j * pieceWidth, (size - 1 - i) * pieceHeight, pieceWidth, pieceHeight));
+                piece.Apply();
+                pieces[i * size + j] = piece;
             }
+        }
+
+        return pieces;
+    }
+
+    // Function to shuffle the puzzle pieces
+    private void ShufflePieces()
+    {
+        int length = puzzlePieces.Length;
+        for (int i = 0; i < length; i++)
+        {
+            int randIndex = Random.Range(i, length);
+            Texture2D temp = puzzlePieces[randIndex];
+            puzzlePieces[randIndex] = puzzlePieces[i];
+            puzzlePieces[i] = temp;
+
+            if (temp == null)
+                emptyIndex = i;
         }
     }
 
-    private bool SwapIfValid(int i, int offset)
+    // Function to create puzzle piece buttons
+    private void CreateButtons()
     {
-        if (((i + offset) >= 0) && ((i + offset) < pieces.Length) && (Mathf.Abs((i + offset) % size - i % size) <= 1))
+        GridLayoutGroup gridLayoutGroup = GetComponent<GridLayoutGroup>();
+        gridLayoutGroup.constraintCount = puzzleSize;
+
+        for (int i = 0; i < puzzlePieces.Length; i++)
         {
-            if (i + offset == emptyLocation)
+            GameObject button = Instantiate(buttonPrefab, transform);
+            button.name = i.ToString();
+            button.GetComponent<RectTransform>().sizeDelta = buttonSize;
+
+            if (puzzlePieces[i] == null)
             {
-                (pieces[i], pieces[i + offset]) = (pieces[i + offset], pieces[i]);
-                emptyLocation = i;
-                return true;
+                emptyIndex = i;
+                continue;
             }
+
+            Image image = button.GetComponent<Image>();
+            image.sprite = Sprite.Create(puzzlePieces[i], new Rect(0, 0, puzzlePieces[i].width, puzzlePieces[i].height), new Vector2(0.5f, 0.5f));
+            int index = i;
+            button.GetComponent<Button>().onClick.AddListener(() => MovePiece(index));
         }
-        return false;
+    }
+
+    // Function to move puzzle piece
+    private void MovePiece(int index)
+    {
+        if (IsAdjacent(index, emptyIndex))
+        {
+            // Swap pieces
+            Texture2D temp = puzzlePieces[index];
+            puzzlePieces[index] = null;
+            puzzlePieces[emptyIndex] = temp;
+
+            // Update empty index
+            emptyIndex = index;
+
+            // Update button images
+            RefreshButtons();
+        }
+    }
+
+    // Function to check if two pieces are adjacent
+    private bool IsAdjacent(int index1, int index2)
+    {
+        int row1 = index1 / puzzleSize;
+        int col1 = index1 % puzzleSize;
+        int row2 = index2 / puzzleSize;
+        int col2 = index2 % puzzleSize;
+
+        return Mathf.Abs(row1 - row2) + Mathf.Abs(col1 - col2) == 1;
+    }
+
+    // Function to refresh button images
+    private void RefreshButtons()
+    {
+        for (int i = 0; i < puzzlePieces.Length; i++)
+        {
+            Image image = transform.GetChild(i).GetComponent<Image>();
+            if (puzzlePieces[i] != null)
+                image.sprite = Sprite.Create(puzzlePieces[i], new Rect(0, 0, puzzlePieces[i].width, puzzlePieces[i].height), new Vector2(0.5f, 0.5f));
+            else
+                image.sprite = null;
+        }
     }
 }
